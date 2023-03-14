136796c109ac141552ecc68ab5742cca0c03225c
Karlis Veckagans <veckaganskarlis@gmail.com>
2023-03-14 19:58:15 +0200

    done

--- src/server.js
+++ src/server.js
@@ -20,16 +20,15 @@ app.use(bodyParser.json());
 // uz servera palaišanu
 database.serialize(() => {
   // create the projects table if it doesn't exist
+
   database.run(`
-    CREATE TABLE IF NOT EXISTS projects (
+    CREATE TABLE IF NOT EXISTS finances (
       id INTEGER PRIMARY KEY,
-      project_name VARCHAR(255) NOT NULL,
-      task VARCHAR(255) NOT NULL,
-      start_date DATE,
-      status ,
-      due_date DATE
+      income INT NOT NULL,
+      expenses INT NOT NULL, 
+      savings INT NOT NULL
     );
-  `);
+  `)
 
 
   database.run(`
@@ -39,6 +38,7 @@ database.serialize(() => {
       password VARCHAR(255) NOT NULL
     );
   `);
+
 
   // if there are no projects in the database yet, add the first one
   database.get('SELECT * FROM users', (err, user) => {
@@ -53,40 +53,25 @@ database.serialize(() => {
 
 
 // Atgriež visus projektus no DB
-app.get('/projects', (req, res) => {
+app.get('/finances', (req, res) => {
     // database.get atgriež tikai vienu pirmo atrasto rezutlātu
       // database.all atgriež visus atrastos rezultātus
-  database.all('SELECT * FROM projects', (error, project) => {
-    res.json(project)
+  database.all('SELECT * FROM finances', (error, finance) => {
+    res.json(finance)
   })
 })
 
-// POST http://localhost:3000/projects
-// pievieno jaunu projektu
-app.post('/projects', (req, res) => {
+
+// POST http://localhost:3000/finances
+// pievieno jaunu finance 
+app.post('/finances', (req, res) => {
   database.run(`
-    INSERT INTO projects (project_name, task, start_date, due_date)
-    VALUES("${req.body.projectName}", "${req.body.task}", "${req.body.start_date}", "${req.body.dueDate}");
+    INSERT INTO finances (income, expenses, savings)
+    VALUES("${req.body.income}", "${req.body.expenses}", "${req.body.savings}");
   `, () => {
-    res.json('Jauns projekts pievienots veiksmīgi')
+    res.json('Jauns ieraksts pievienots veiksmīgi')
   })
 })
-
-app.put('/projects/:id', (req, res) => {
-  database.run(`
-  UPDATE projects SET project_name = "${req.body.projectName}", task = "${req.body.task}", start_date = "${req.body.start_date}", due_date = "${req.body.dueDate}" 
-  WHERE id=${req.params.id};
-  `, () => {
-    res.json('Jauns projekts pievienots veiksmīgi')
-  })
-})
-
-app.delete('/projects/:id', (req, res) => {
-  database.run(`DELETE FROM projects WHERE id=${req.params.id}`,
-  () => {
-    res.json('Projekts dzests!')
-  })
-}) 
 
 // users
 app.get('/users', (req, res) => {
@@ -110,4 +95,5 @@ app.post('/users', (req, res) => {
 app.listen(3004, () => {
   console.log(`Example app listening on port 3004`)
 })
+
 
